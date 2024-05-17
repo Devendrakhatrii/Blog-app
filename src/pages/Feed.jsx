0f7b02755d13ad3287734059fc1a1f0b7f48d714
Devendra <devendrakhatri904@gmail.com>
2024-05-17 22:23:18 +0545

    created feed navbar

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -4,13 +4,13 @@ import { BadgePlus } from "lucide-react";
 const Feed = () => {
   return (
     <>
-      <div className="bg-red-500 h-screen w-1/2 ml-60 mt-3">
-        <nav className=" flex items-center h-10 m-4 p-4 border-b-2   flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+      <div className="    h-screen w-1/2 ml-60 mt-3 ">
+        <nav className="z-999 sticky top-0 flex items-center h-10 m-4 py-6 p-4 border-b-2   flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link
             href="#"
-            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+            className="flex items-center gap-2 text-lg font-semibold md:text-base text-muted-foreground transition-colors hover:text-foreground"
           >
-            <BadgePlus className="h-4 w-4" />
+            <BadgePlus className="ml-2 -mr-3  h-4 w-4" />
             <span className="sr-only">Acme Inc</span>
           </Link>
           <Link
