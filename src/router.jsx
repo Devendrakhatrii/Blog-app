ef53a8dfef65b7c77b4c1a18efb46d0895fab74d
Devendra <devendrakhatri904@gmail.com>
2024-05-17 23:04:56 +0545

    added layout file to make navbar appear in all page

--- src/router.jsx
+++ src/router.jsx
@@ -4,26 +4,35 @@ import { HomePage } from "@/pages/Home";
 import { LoginPage } from "@/pages/Login";
 import { SignupPage } from "@/pages/Signup";
 import { NavigationPage } from "@/pages/Navigation";
+import Navbar from "./components/Navbar";
+import Layout from "./Layout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <NavigationPage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/signup",
-    element: <SignupPage />,
-  },
-  {
-    path: "/dashboard",
-    element: <DashboardPage />,
-  },
-  {
-    path: "/home",
-    element: <HomePage />,
+    element: <Layout />,
+    children: [
+      { path: "", element: <>Home</> },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "/dashboard",
+        element: <DashboardPage />,
+      },
+      {
+        path: "/home",
+        element: <HomePage />,
+      },
+      {
+        path: "/navbar",
+        element: <Navbar />,
+      },
+    ],
   },
 ]);
